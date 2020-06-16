b79c24917a54c1ce34e2f547328f615c549957e2
Charles Markello <cmarkell@ucsc.edu>
2020-06-15 21:22:50 -0700

    Debugged eagle dockerfile

---
+++ misc_containers/eagle2.4/docker_build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget https://data.broadinstitute.org/alkesgroup/Eagle/downloads/Eagle_v2.4.1.tar.gz
+tar xvzf Eagle_v2.4.1.tar.gz
+docker build -t quay.io/cmarkello/eagle .
+rm -f Eagle_v2.4.1 Eagle_v2.4.1.tar.gz
