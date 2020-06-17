81fe4dd67924d86a95714c8f20e40535c1dd9a07
Charles Markello <cmarkell@ucsc.edu>
2020-06-16 20:34:02 -0700

    Fix source removal in docker_build.sh script

--- misc_containers/eagle2.4/docker_build.sh
+++ misc_containers/eagle2.4/docker_build.sh
@@ -2,4 +2,4 @@
 wget https://data.broadinstitute.org/alkesgroup/Eagle/downloads/Eagle_v2.4.1.tar.gz
 tar xvzf Eagle_v2.4.1.tar.gz
 docker build -t quay.io/cmarkello/eagle .
-rm -f Eagle_v2.4.1 Eagle_v2.4.1.tar.gz
+rm -fr Eagle_v2.4.1 Eagle_v2.4.1.tar.gz
