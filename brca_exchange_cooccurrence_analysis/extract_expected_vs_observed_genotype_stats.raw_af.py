b51a63b53e7b629a4f5290303171794d8564ee18
Charles Markello <cmarkell@ucsc.edu>
2020-08-27 15:04:03 -0700

    Finished exp vs obs genotype stat script. Improved combined homozygous vs heterozygous scatter plots.

--- brca_exchange_cooccurrence_analysis/extract_expected_vs_observed_genotype_stats.raw_af.py
+++ brca_exchange_cooccurrence_analysis/extract_expected_vs_observed_genotype_stats.raw_af.py
@@ -1,12 +1,5 @@
-import matplotlib
-matplotlib.use('Agg')
 import vcf, argparse, sys
-import numpy as np
-import pandas as pd
-import math
-from scipy.stats import chisquare
 from collections import defaultdict
-import matplotlib.pyplot
 import ast
 
 def parse_args():
@@ -21,8 +14,6 @@ def parse_args():
         help='Input vcf filepath.')
     parser.add_argument('-v', '--inHOMALT', type=str,
         help='Input list of samples that contain at least 1 homozygous alt VUS genotype.')
-    parser.add_argument('-o', '--outReport', type=str,
-        help='Output report filename.')
 
     options = parser.parse_args()
     return options
@@ -45,7 +36,7 @@ def main(args):
     vus_variant_hom_genotype_count = defaultdict(tuple)
     vus_variant_lt_0_01_hom_genotype_count = defaultdict(tuple)
     vus_variant_lt_0_005_hom_genotype_count = defaultdict(tuple)
-    vus_variant_lt_0_001_hom_genotype_count = defaultdicti(tuple)
+    vus_variant_lt_0_001_hom_genotype_count = defaultdict(tuple)
     num_samples = 0
     for record in vcf_reader:
         num_hom_alts = len(record.get_hom_alts())
@@ -70,10 +61,65 @@ def main(args):
                 vus_variant_lt_0_001_hom_genotype_count[variant_record] = (num_hom_alts, minor_af)
     
     expected_hom_genotypes_all = 0.0
+    expected_proportion_all = 0.0
     observed_hom_genotypes_all = 0.0
-    for variant in all_variant_hom_genotype_count.keys():
-        
-        
+    for (hom_alt_count,minor_af) in all_variant_hom_genotype_count.values():
+        observed_hom_genotypes_all += hom_alt_count
+        expected_proportion_all += minor_af**2
+    expected_hom_genotypes_all = expected_proportion_all*num_samples
+    
+    expected_hom_genotypes_vus = 0.0
+    expected_proportion_vus = 0.0
+    observed_hom_genotypes_vus = 0.0
+    for (hom_alt_count,minor_af) in vus_variant_hom_genotype_count.values():
+        observed_hom_genotypes_vus += hom_alt_count
+        expected_proportion_vus += minor_af**2
+    expected_hom_genotypes_vus = expected_proportion_vus*num_samples
+    
+    expected_hom_genotypes_vus_lt_0_01 = 0.0
+    expected_proportion_vus_lt_0_01 = 0.0
+    observed_hom_genotypes_vus_lt_0_01 = 0.0
+    for (hom_alt_count,minor_af) in vus_variant_lt_0_01_hom_genotype_count.values():
+        observed_hom_genotypes_vus_lt_0_01 += hom_alt_count
+        expected_proportion_vus_lt_0_01 += minor_af**2
+    expected_hom_genotypes_vus_lt_0_01 = expected_proportion_vus_lt_0_01*num_samples
+    
+    expected_hom_genotypes_vus_lt_0_005 = 0.0
+    expected_proportion_vus_lt_0_005 = 0.0
+    observed_hom_genotypes_vus_lt_0_005 = 0.0
+    for (hom_alt_count,minor_af) in vus_variant_lt_0_005_hom_genotype_count.values():
+        observed_hom_genotypes_vus_lt_0_005 += hom_alt_count
+        expected_proportion_vus_lt_0_005 += minor_af**2
+    expected_hom_genotypes_vus_lt_0_005 = expected_proportion_vus_lt_0_005*num_samples
+    
+    expected_hom_genotypes_vus_lt_0_001 = 0.0
+    expected_proportion_vus_lt_0_001 = 0.0
+    observed_hom_genotypes_vus_lt_0_001 = 0.0
+    for (hom_alt_count,minor_af) in vus_variant_lt_0_001_hom_genotype_count.values():
+        observed_hom_genotypes_vus_lt_0_001 += hom_alt_count
+        expected_proportion_vus_lt_0_001 += minor_af**2
+    expected_hom_genotypes_vus_lt_0_001 = expected_proportion_vus_lt_0_001*num_samples
+    
+    print('** ALL GENOTYPES **')
+    print('expected number of homozygous alt genotypes: {}'.format(expected_hom_genotypes_all))
+    print('observed number of homozygous alt genotypes: {}'.format(observed_hom_genotypes_all))
+    
+    print('** ALL VUS GENOTYPES **')
+    print('expected number of homozygous alt genotypes: {}'.format(expected_hom_genotypes_vus))
+    print('observed number of homozygous alt genotypes: {}'.format(observed_hom_genotypes_vus))
+    
+    print('** VUS WITH AF <= 0.01 GENOTYPES **')
+    print('expected number of homozygous alt genotypes: {}'.format(expected_hom_genotypes_vus_lt_0_01))
+    print('observed number of homozygous alt genotypes: {}'.format(observed_hom_genotypes_vus_lt_0_01))
+    
+    print('** VUS WITH AF <= 0.005 GENOTYPES **')
+    print('expected number of homozygous alt genotypes: {}'.format(expected_hom_genotypes_vus_lt_0_005))
+    print('observed number of homozygous alt genotypes: {}'.format(observed_hom_genotypes_vus_lt_0_005))
+    
+    print('** VUS WITH AF <= 0.001 GENOTYPES **')
+    print('expected number of homozygous alt genotypes: {}'.format(expected_hom_genotypes_vus_lt_0_001))
+    print('observed number of homozygous alt genotypes: {}'.format(observed_hom_genotypes_vus_lt_0_001))
+    
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
 
