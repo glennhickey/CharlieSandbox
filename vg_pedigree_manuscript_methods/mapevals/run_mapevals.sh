7886dcdc102ef6f80a5c4de7186b975838e01e2e
Charles Markello <cmarkell@ucsc.edu>
2021-07-13 20:50:02 -0400

    vg pedigree paper mapeval method development in progress

--- vg_pedigree_manuscript_methods/mapevals/run_mapevals.sh
+++ vg_pedigree_manuscript_methods/mapevals/run_mapevals.sh
@@ -1,119 +1,123 @@
 #!/bin/bash
 
-# RUN BWAMEM
 WORK_DIR="/data/Udpbinfo/usr/markellocj/vg_trio_methods/redo_mapevals/HG002_sim_reads"
 #SIMULATED_READ_DIR="."
 #SIMULATED_READ_DIR="high_conf_hg002_v4.2.1_regions"
 #SIMULATED_READ_DIR="all_difficult_regions_hg002_v4.2.1_regions"
-SIMULATED_READ_DIR="alllowmapandsegdupregions_hg002_v4.2.1_regions"
-
+#SIMULATED_READ_DIR="alllowmapandsegdupregions_hg002_v4.2.1_regions"
+#SIMULATED_READ_DIR="mhc_hg002_v4.2.1_regions"
+SIMULATED_READ_DIR="cmrg_hg002_v4.2.1_regions"
 cd ${WORK_DIR}
 module load samtools bwa singularity python
+
+# RUN BWAMEM
 time bwa mem -t 32 -p /data/markellocj/fasta_references/grch38_reference/GCA_000001405.15_GRCh38_no_alt_plus_hs38d1_analysis_set.compact_decoys.fna ${WORK_DIR}/${SIMULATED_READ_DIR}/sim.paired.fq > mapped.bam
 
 time samtools view -F 2048 -b mapped.bam > mapped.primary.bam
 time samtools view -f 2048 -b mapped.bam > mapped.secondary.bam
 
 singularity exec --env SIMULATED_READ_DIR=${SIMULATED_READ_DIR} -H ${PWD}:${HOME} --pwd ${HOME} docker://quay.io/vgteam/vg:v1.31.0 \
-/bin/bash -c 'set -eo pipefail && \
-SIMULATED_READ_DIR=${SIMULATED_READ_DIR} && \
-time vg inject -x hg002_sample_grch38.xg mapped.primary.bam > mapped.primary.gam && \
-time vg inject -x hg002_sample_grch38.xg mapped.secondary.bam > mapped.secondary.gam && \
-time vg view -aj mapped.primary.gam | sed "s/\/1/_1/g" | sed "s/\/2/_2/g" | vg view -aGJ - | vg annotate -m -x hg002_sample_grch38.xg -a - | vg gamcompare -r 100 -s - ${SIMULATED_READ_DIR}/sim.gam 2> count | vg view -aj - > compared.primary.json && \
+/bin/bash -cx 'set -eo pipefail ; \
+SIMULATED_READ_DIR=${SIMULATED_READ_DIR} ; \
+time vg inject -x hg002_sample_grch38.xg mapped.primary.bam > mapped.primary.gam ; \
+time vg inject -x hg002_sample_grch38.xg mapped.secondary.bam > mapped.secondary.gam ; \
+time vg view -aj mapped.primary.gam | sed "s/\/1/_1/g" | sed "s/\/2/_2/g" | vg view -aGJ - | vg annotate -m -x hg002_sample_grch38.xg -a - | vg gamcompare -r 100 -s - ${SIMULATED_READ_DIR}/sim.gam 2> count | vg view -aj - > compared.primary.json ; \
 time vg view -aj mapped.secondary.gam | sed "s/\/1/_1/g" | sed "s/\/2/_2/g" | vg view -aGJ - | vg annotate -m -x hg002_sample_grch38.xg -a - | vg gamcompare -r 100 - ${SIMULATED_READ_DIR}/sim.gam | vg view -aj - > compared.secondary.json'
 
 time python /data/markellocj/git/giraffe-sv-paper/scripts/mapping/combine_reads.py compared.primary.json compared.secondary.json bwamem.compared.json
 
-singularity exec -H ${PWD}:${HOME} --pwd ${HOME} docker://quay.io/vgteam/vg:v1.31.0 \
-/bin/bash -c 'set -eo pipefail && \
-sed -i "/^$/d" bwamem.compared.json && \
-printf "correct\tmq\tscore\taligner\n" > bwamem_roc_stats.tsv && \
-READS="HG002" && \
-PAIRING="paired" && \
-GRAPH="hg002_sample_grch38" && \
-ALGORITHM="bwamem" && \
-CORRECT_COUNT="$(grep correctly_mapped bwamem.compared.json | wc -l)" && \
-SCORE="$(sed -n "2p" count | sed "s/[^0-9\.]//g")" && \
-MAPQ="$(grep mapping_quality\":\ 60 bwamem.compared.json | wc -l)" && \
-MAPQ60="$(grep -v correctly_mapped bwamem.compared.json | grep mapping_quality\":\ 60 | wc -l)" && \
-IDENTITY=$(jq ".identity" bwamem.compared.json | awk "{sum+=\$1} END {print sum/NR}") && \
-echo ${GRAPH} ${READS} ${PAIRING} ${CORRECT_COUNT} ${MAPQ} ${MAPQ60} ${SCORE} && \
-printf "graph\talgorithm\treads\tspeed\tpairing\tcorrect\tmapq60\twrong_mapq60\tidentity\tscore\n" > report_bwamem.tsv && \
-printf "${GRAPH}\t${ALGORITHM}\t${READS}\t${PAIRING}\t-\t${CORRECT_COUNT}\t${MAPQ}\t${MAPQ60}\t${IDENTITY}\t${SCORE}\n" >> report_bwamem.tsv && \
-jq -r "(if .correctly_mapped then 1 else 0 end|tostring) + \",\" + (.mapping_quality|tostring) + \",\" + (.score|tostring)" bwamem.compared.json | sed "s/\,/\t/g" | sed "s/$/\tbwamem_${GRAPH}${READS}${PAIRING}/" | sed "s/single//g ; s/paired/-pe/g ; s/null/0/g" >> bwamem_roc_stats.tsv'
+singularity exec --env SIMULATED_READ_DIR=${SIMULATED_READ_DIR} -H ${PWD}:${HOME} --pwd ${HOME} docker://quay.io/vgteam/vg:v1.31.0 \
+/bin/bash -cx 'set -o pipefail ; \
+sed -i "/^$/d" bwamem.compared.json ; \
+printf "correct\tmq\tscore\taligner\n" > bwamem_roc_stats.${SIMULATED_READ_DIR}.tsv ; \
+READS="HG002" ; \
+PAIRING="paired" ; \
+GRAPH="hg002_sample_grch38" ; \
+ALGORITHM="bwamem" ; \
+CORRECT_COUNT="$(grep correctly_mapped bwamem.compared.json | wc -l)"; \
+SCORE="$(sed -n "2p" count | sed "s/[^0-9\.]//g")"; \
+MAPQ="$(grep mapping_quality\":\ 60 bwamem.compared.json | wc -l)"; \
+echo test ; \
+MAPQ60="$(grep -v correctly_mapped bwamem.compared.json | grep mapping_quality\":\ 60 | wc -l)"; \
+echo test2 ; \
+IDENTITY=$(jq ".identity" bwamem.compared.json | awk "{sum+=\$1} END {print sum/NR}"); \
+echo ${GRAPH} ${READS} ${PAIRING} ${CORRECT_COUNT} ${MAPQ} ${MAPQ60} ${SCORE}; \
+printf "graph\talgorithm\treads\tspeed\tpairing\tcorrect\tmapq60\twrong_mapq60\tidentity\tscore\n" > report_bwamem.${SIMULATED_READ_DIR}.tsv; \
+printf "${GRAPH}\t${ALGORITHM}\t${READS}\t${PAIRING}\t-\t${CORRECT_COUNT}\t${MAPQ}\t${MAPQ60}\t${IDENTITY}\t${SCORE}\n" >> report_bwamem.${SIMULATED_READ_DIR}.tsv; \
+jq -r "(if .correctly_mapped then 1 else 0 end|tostring) + \",\" + (.mapping_quality|tostring) + \",\" + (.score|tostring)" bwamem.compared.json | sed "s/\,/\t/g" | sed "s/$/\tbwamem_${GRAPH}${READS}${PAIRING}/" | sed "s/single//g ; s/paired/-pe/g ; s/null/0/g" >> bwamem_roc_stats.${SIMULATED_READ_DIR}.tsv'
 
 # RUN GIRAFFE PRIMARY
-singularity --env SIMULATED_READ_DIR=${SIMULATED_READ_DIR} -H ${PWD}:${HOME} --pwd ${HOME} \
+singularity exec --env SIMULATED_READ_DIR=${SIMULATED_READ_DIR} -H ${PWD}:${HOME} --pwd ${HOME} \
 -B /data/markellocj/graph_reference/snp1kg_ref_wgs/grch38_compact_decoys_primary_ref:${HOME}/grch38_compact_decoys_primary_ref docker://quay.io/vgteam/vg:v1.31.0 \
-/bin/bash -c 'set -eo pipefail && \
-PARAM_PRESET="default" && \
-PAIRED="-i" && \
-time vg giraffe -x grch38_compact_decoys_primary_ref/primarygrch38_no_alt_plus_hs38d1_v1.27.0_90_ga64b70c1f.xg -H grch38_compact_decoys_primary_ref/primarygrch38_no_alt_plus_hs38d1_v1.27.0_90_ga64b70c1f.gbwt -m grch38_compact_decoys_primary_ref/primarygrch38_no_alt_plus_hs38d1_v1.27.0_90_ga64b70c1f.min -d grch38_compact_decoys_primary_ref/primarygrch38_no_alt_plus_hs38d1_v1.27.0_90_ga64b70c1f.dist -G ${SIMULATED_READ_DIR}/sim.gam -b ${PARAM_PRESET} ${PAIRED} -t 22 > mapped.primary.gam && \
-time vg gamcompare -r 100 -s <(vg annotate -m -x grch38_compact_decoys_primary_ref/primarygrch38_no_alt_plus_hs38d1_v1.27.0_90_ga64b70c1f.xg -a mapped.primary.gam) ${SIMULATED_READ_DIR}/sim.gam 2>count | vg view -aj - > giraffe_primary.compared.json && \
-GRAPH="hg002_sample_grch38" && \
-GBWT="sampled.64" && \
-READS="HG002" && \
-PARAM_PRESET="default" && \
-PAIRING="paired" && \
-CORRECT_COUNT="$(sed -n "1p" count | sed "s/[^0-9]//g")" && \
-SCORE="$(sed -n "2p" count | sed "s/[^0-9\.]//g")" && \
-MAPQ="$(grep mapping_quality\":\ 60 giraffe_primary.compared.json | wc -l)" && \
-MAPQ60="$(grep -v correctly_mapped giraffe_primary.compared.json | grep mapping_quality\":\ 60 | wc -l)" && \
-IDENTITY=$(jq ".identity" giraffe_primary.compared.json | awk "{sum+=\$1} END {print sum/NR}") && \
-echo ${GRAPH} ${GBWT} ${READS} ${PARAM_PRESET}${PAIRING} ${CORRECT_COUNT} ${MAPQ} ${MAPQ60} ${IDENTITY} ${SCORE} && \
-printf "graph\tgbwt\treads\tpairing\tspeed\tcorrect\tmapq60\twrong_mapq60\tidentity\tscore\n" > giraffe_primary_report.tsv && \
-printf "${GRAPH}\t${GBWT}\t${READS}\t${PARAM_PRESET}\t${PAIRING}\t${CORRECT_COUNT}\t${MAPQ}\t${MAPQ60}\t${IDENTITY}\t${SCORE}\n" >> giraffe_primary_report.tsv && \
-printf "correct\tmq\tscore\taligner\n" > giraffe_primary_roc_stats.tsv && \
-jq -r "(if .correctly_mapped then 1 else 0 end|tostring) + \",\" + (.mapping_quality|tostring) + \",\" + (.score|tostring)" giraffe_primary.compared.json | sed "s/\,/\t/g" | sed "s/$/\tgiraffe_primary_${PARAM_PRESET}_${GRAPH}${GBWT}${READS}${PAIRING}/" >> giraffe_primary_roc_stats.tsv && \
-sed -i "s/single//g ; s/paired/-pe/g ; s/null/0/g" giraffe_primary_roc_stats.tsv'
+/bin/bash -cx 'set -o pipefail ; \
+PARAM_PRESET="default" ; \
+PAIRED="-i" ; \
+time vg giraffe -x grch38_compact_decoys_primary_ref/primarygrch38_no_alt_plus_hs38d1_v1.27.0_90_ga64b70c1f.xg -H grch38_compact_decoys_primary_ref/primarygrch38_no_alt_plus_hs38d1_v1.27.0_90_ga64b70c1f.gbwt -m grch38_compact_decoys_primary_ref/primarygrch38_no_alt_plus_hs38d1_v1.27.0_90_ga64b70c1f.min -d grch38_compact_decoys_primary_ref/primarygrch38_no_alt_plus_hs38d1_v1.27.0_90_ga64b70c1f.dist -G ${SIMULATED_READ_DIR}/sim.gam -b ${PARAM_PRESET} ${PAIRED} -t 22 > mapped.primary.gam ; \
+time vg gamcompare -r 100 -s <(vg annotate -m -x grch38_compact_decoys_primary_ref/primarygrch38_no_alt_plus_hs38d1_v1.27.0_90_ga64b70c1f.xg -a mapped.primary.gam) ${SIMULATED_READ_DIR}/sim.gam 2>count | vg view -aj - > giraffe_primary.compared.json ; \
+GRAPH="hg002_sample_grch38" ; \
+GBWT="sampled.64" ; \
+READS="HG002" ; \
+PARAM_PRESET="default" ; \
+PAIRING="paired" ; \
+CORRECT_COUNT="$(sed -n "1p" count | sed "s/[^0-9]//g")" ; \
+SCORE="$(sed -n "2p" count | sed "s/[^0-9\.]//g")" ; \
+MAPQ="$(grep mapping_quality\":\ 60 giraffe_primary.compared.json | wc -l)" ; \
+MAPQ60="$(grep -v correctly_mapped giraffe_primary.compared.json | grep mapping_quality\":\ 60 | wc -l)" ; \
+IDENTITY=$(jq ".identity" giraffe_primary.compared.json | awk "{sum+=\$1} END {print sum/NR}") ; \
+echo ${GRAPH} ${GBWT} ${READS} ${PARAM_PRESET}${PAIRING} ${CORRECT_COUNT} ${MAPQ} ${MAPQ60} ${IDENTITY} ${SCORE} ; \
+printf "graph\tgbwt\treads\tpairing\tspeed\tcorrect\tmapq60\twrong_mapq60\tidentity\tscore\n" > giraffe_primary_report.${SIMULATED_READ_DIR}.tsv ; \
+printf "${GRAPH}\t${GBWT}\t${READS}\t${PARAM_PRESET}\t${PAIRING}\t${CORRECT_COUNT}\t${MAPQ}\t${MAPQ60}\t${IDENTITY}\t${SCORE}\n" >> giraffe_primary_report.${SIMULATED_READ_DIR}.tsv ; \
+printf "correct\tmq\tscore\taligner\n" > giraffe_primary_roc_stats.${SIMULATED_READ_DIR}.tsv ; \
+jq -r "(if .correctly_mapped then 1 else 0 end|tostring) + \",\" + (.mapping_quality|tostring) + \",\" + (.score|tostring)" giraffe_primary.compared.json | sed "s/\,/\t/g" | sed "s/$/\tgiraffe_primary_${PARAM_PRESET}_${GRAPH}${GBWT}${READS}${PAIRING}/" >> giraffe_primary_roc_stats.${SIMULATED_READ_DIR}.tsv ; \
+sed -i "s/single//g ; s/paired/-pe/g ; s/null/0/g" giraffe_primary_roc_stats.${SIMULATED_READ_DIR}.tsv'
 
 # RUN GIRAFFE 1000GP
-singularity shell --env SIMULATED_READ_DIR=${SIMULATED_READ_DIR} -H ${PWD}:${HOME} --pwd ${HOME} \
+singularity exec --env SIMULATED_READ_DIR=${SIMULATED_READ_DIR} -H ${PWD}:${HOME} --pwd ${HOME} \
 -B /data/markellocj/graph_reference/snp1kg_ref_wgs/grch38_liftover_snp1kg_nosegdup/all_samples:${HOME}/all_samples docker://quay.io/vgteam/vg:v1.31.0 \
-/bin/bash -c 'set -eo pipefail && \
-PARAM_PRESET="default" && \
-PAIRED="-i" && \
-time vg giraffe -x all_samples/liftover_snp1kg_grch38_nosegdup.xg -H all_samples/liftover_snp1kg_grch38_nosegdup.gbwt -m all_samples/liftover_snp1kg_grch38_nosegdup.min -d all_samples/liftover_snp1kg_grch38_nosegdup.dist -G ${SIMULATED_READ_DIR}/sim.gam -b ${PARAM_PRESET} ${PAIRED} -t 22 > mapped.snp1kg.gam && \
-time vg gamcompare -r 100 -s <(vg annotate -m -x all_samples/liftover_snp1kg_grch38_nosegdup.xg -a mapped.snp1kg.gam) ${SIMULATED_READ_DIR}/sim.gam 2>count | vg view -aj - > giraffe_snp1kg.compared.json && \
-GRAPH="hg002_sample_grch38" && \
-GBWT="sampled.64" && \
-READS="HG002" && \
-PARAM_PRESET="default" && \
-PAIRING="paired" && \
-CORRECT_COUNT="$(sed -n "1p" count | sed "s/[^0-9]//g")" && \
-SCORE="$(sed -n "2p" count | sed "s/[^0-9\.]//g")" && \
-MAPQ="$(grep mapping_quality\":\ 60 giraffe_snp1kg.compared.json | wc -l)" && \
-MAPQ60="$(grep -v correctly_mapped giraffe_snp1kg.compared.json | grep mapping_quality\":\ 60 | wc -l)" && \
-IDENTITY=$(jq ".identity" giraffe_snp1kg.compared.json | awk "{sum+=\$1} END {print sum/NR}") && \
-echo ${GRAPH} ${GBWT} ${READS} ${PARAM_PRESET}${PAIRING} ${CORRECT_COUNT} ${MAPQ} ${MAPQ60} ${IDENTITY} ${SCORE} && \
-printf "graph\tgbwt\treads\tpairing\tspeed\tcorrect\tmapq60\twrong_mapq60\tidentity\tscore\n" > giraffe_snp1kg_report.tsv && \
-printf "${GRAPH}\t${GBWT}\t${READS}\t${PARAM_PRESET}\t${PAIRING}\t${CORRECT_COUNT}\t${MAPQ}\t${MAPQ60}\t${IDENTITY}\t${SCORE}\n" >> giraffe_snp1kg_report.tsv && \
-printf "correct\tmq\tscore\taligner\n" > giraffe_snp1kg_roc_stats.tsv && \
-jq -r "(if .correctly_mapped then 1 else 0 end|tostring) + \",\" + (.mapping_quality|tostring) + \",\" + (.score|tostring)" giraffe_snp1kg.compared.json | sed "s/\,/\t/g" | sed "s/$/\tgiraffe_snp1kg_${PARAM_PRESET}_${GRAPH}${GBWT}${READS}${PAIRING}/" >> giraffe_snp1kg_roc_stats.tsv && \
-sed -i "s/single//g ; s/paired/-pe/g ; s/null/0/g" giraffe_snp1kg_roc_stats.tsv'
+/bin/bash -cx 'set -o pipefail ; \
+PARAM_PRESET="default" ; \
+PAIRED="-i" ; \
+time vg giraffe -x all_samples/liftover_snp1kg_grch38_nosegdup.xg -H all_samples/liftover_snp1kg_grch38_nosegdup.gbwt -m all_samples/liftover_snp1kg_grch38_nosegdup.min -d all_samples/liftover_snp1kg_grch38_nosegdup.dist -G ${SIMULATED_READ_DIR}/sim.gam -b ${PARAM_PRESET} ${PAIRED} -t 22 > mapped.snp1kg.gam ; \
+time vg gamcompare -r 100 -s <(vg annotate -m -x all_samples/liftover_snp1kg_grch38_nosegdup.xg -a mapped.snp1kg.gam) ${SIMULATED_READ_DIR}/sim.gam 2>count | vg view -aj - > giraffe_snp1kg.compared.json ; \
+GRAPH="hg002_sample_grch38" ; \
+GBWT="sampled.64" ; \
+READS="HG002" ; \
+PARAM_PRESET="default" ; \
+PAIRING="paired" ; \
+CORRECT_COUNT="$(sed -n "1p" count | sed "s/[^0-9]//g")" ; \
+SCORE="$(sed -n "2p" count | sed "s/[^0-9\.]//g")" ; \
+MAPQ="$(grep mapping_quality\":\ 60 giraffe_snp1kg.compared.json | wc -l)" ; \
+MAPQ60="$(grep -v correctly_mapped giraffe_snp1kg.compared.json | grep mapping_quality\":\ 60 | wc -l)" ; \
+IDENTITY=$(jq ".identity" giraffe_snp1kg.compared.json | awk "{sum+=\$1} END {print sum/NR}") ; \
+echo ${GRAPH} ${GBWT} ${READS} ${PARAM_PRESET}${PAIRING} ${CORRECT_COUNT} ${MAPQ} ${MAPQ60} ${IDENTITY} ${SCORE} ; \
+printf "graph\tgbwt\treads\tpairing\tspeed\tcorrect\tmapq60\twrong_mapq60\tidentity\tscore\n" > giraffe_snp1kg_report.${SIMULATED_READ_DIR}.tsv ; \
+printf "${GRAPH}\t${GBWT}\t${READS}\t${PARAM_PRESET}\t${PAIRING}\t${CORRECT_COUNT}\t${MAPQ}\t${MAPQ60}\t${IDENTITY}\t${SCORE}\n" >> giraffe_snp1kg_report.${SIMULATED_READ_DIR}.tsv ; \
+printf "correct\tmq\tscore\taligner\n" > giraffe_snp1kg_roc_stats.${SIMULATED_READ_DIR}.tsv ; \
+jq -r "(if .correctly_mapped then 1 else 0 end|tostring) + \",\" + (.mapping_quality|tostring) + \",\" + (.score|tostring)" giraffe_snp1kg.compared.json | sed "s/\,/\t/g" | sed "s/$/\tgiraffe_snp1kg_${PARAM_PRESET}_${GRAPH}${GBWT}${READS}${PAIRING}/" >> giraffe_snp1kg_roc_stats.${SIMULATED_READ_DIR}.tsv ; \
+sed -i "s/single//g ; s/paired/-pe/g ; s/null/0/g" giraffe_snp1kg_roc_stats.${SIMULATED_READ_DIR}.tsv'
 
 # RUN GIRAFFE PARENTAL
-singularity shell --env SIMULATED_READ_DIR=${SIMULATED_READ_DIR} -H ${PWD}:${HOME} --pwd ${HOME} \
+singularity exec --env SIMULATED_READ_DIR=${SIMULATED_READ_DIR} -H ${PWD}:${HOME} --pwd ${HOME} \
 -B /data/markellocj/graph_reference/parental_ref_deeptrio_wgs_grch38_nosegdup_v1.31.0/hg002_cohort:${HOME}/hg002_cohort docker://quay.io/vgteam/vg:v1.31.0 \
-/bin/bash -c 'set -eo pipefail && \
-PARAM_PRESET="default" && \
-PAIRED="-i" && \
-time vg giraffe -x hg002_cohort/HG002.parental.graphs.xg -H hg002_cohort/HG002.parental.graphs.gbwt -m hg002_cohort/HG002.parental.graphs.min -d hg002_cohort/HG002.parental.graphs.dist -G ${SIMULATED_READ_DIR}/sim.gam -b ${PARAM_PRESET} ${PAIRED} -t 22 > mapped.parental.gam && \
-time vg gamcompare -r 100 -s <(vg annotate -m -x hg002_cohort/HG002.parental.graphs.xg -a mapped.parental.gam) ${SIMULATED_READ_DIR}/sim.gam 2>count | vg view -aj - > giraffe_parental.compared.json && \
-GRAPH="hg002_sample_grch38" && \
-GBWT="sampled.64" && \
-READS="HG002" && \
-PARAM_PRESET="default" && \
-PAIRING="paired" && \
-CORRECT_COUNT="$(sed -n "1p" count | sed "s/[^0-9]//g")" && \
-SCORE="$(sed -n "2p" count | sed "s/[^0-9\.]//g")" && \
-MAPQ="$(grep mapping_quality\":\ 60 giraffe_parental.compared.json | wc -l)" && \
-MAPQ60="$(grep -v correctly_mapped giraffe_parental.compared.json | grep mapping_quality\":\ 60 | wc -l)" && \
-IDENTITY=$(jq ".identity" giraffe_parental.compared.json | awk "{sum+=\$1} END {print sum/NR}") && \
-echo ${GRAPH} ${GBWT} ${READS} ${PARAM_PRESET}${PAIRING} ${CORRECT_COUNT} ${MAPQ} ${MAPQ60} ${IDENTITY} ${SCORE} && \
-printf "graph\tgbwt\treads\tpairing\tspeed\tcorrect\tmapq60\twrong_mapq60\tidentity\tscore\n" > giraffe_parental_report.tsv && \
-printf "${GRAPH}\t${GBWT}\t${READS}\t${PARAM_PRESET}\t${PAIRING}\t${CORRECT_COUNT}\t${MAPQ}\t${MAPQ60}\t${IDENTITY}\t${SCORE}\n" >> giraffe_parental_report.tsv && \
-printf "correct\tmq\tscore\taligner\n" > giraffe_parental_roc_stats.tsv && \
-jq -r "(if .correctly_mapped then 1 else 0 end|tostring) + \",\" + (.mapping_quality|tostring) + \",\" + (.score|tostring)" giraffe_parental.compared.json | sed "s/\,/\t/g" | sed "s/$/\tgiraffe_parental_${PARAM_PRESET}_${GRAPH}${GBWT}${READS}${PAIRING}/" >> giraffe_parental_roc_stats.tsv && \
-sed -i "s/single//g ; s/paired/-pe/g ; s/null/0/g" giraffe_parental_roc_stats.tsv'
+/bin/bash -cx 'set -o pipefail ; \
+PARAM_PRESET="default" ; \
+PAIRED="-i" ; \
+time vg giraffe -x hg002_cohort/HG002.parental.graphs.xg -H hg002_cohort/HG002.parental.graphs.gbwt -m hg002_cohort/HG002.parental.graphs.min -d hg002_cohort/HG002.parental.graphs.dist -G ${SIMULATED_READ_DIR}/sim.gam -b ${PARAM_PRESET} ${PAIRED} -t 22 > mapped.parental.gam ; \
+time vg gamcompare -r 100 -s <(vg annotate -m -x hg002_cohort/HG002.parental.graphs.xg -a mapped.parental.gam) ${SIMULATED_READ_DIR}/sim.gam 2>count | vg view -aj - > giraffe_parental.compared.json ; \
+GRAPH="hg002_sample_grch38" ; \
+GBWT="sampled.64" ; \
+READS="HG002" ; \
+PARAM_PRESET="default" ; \
+PAIRING="paired" ; \
+CORRECT_COUNT="$(sed -n "1p" count | sed "s/[^0-9]//g")" ; \
+SCORE="$(sed -n "2p" count | sed "s/[^0-9\.]//g")" ; \
+MAPQ="$(grep mapping_quality\":\ 60 giraffe_parental.compared.json | wc -l)" ; \
+MAPQ60="$(grep -v correctly_mapped giraffe_parental.compared.json | grep mapping_quality\":\ 60 | wc -l)" ; \
+IDENTITY=$(jq ".identity" giraffe_parental.compared.json | awk "{sum+=\$1} END {print sum/NR}") ; \
+echo ${GRAPH} ${GBWT} ${READS} ${PARAM_PRESET}${PAIRING} ${CORRECT_COUNT} ${MAPQ} ${MAPQ60} ${IDENTITY} ${SCORE} ; \
+printf "graph\tgbwt\treads\tpairing\tspeed\tcorrect\tmapq60\twrong_mapq60\tidentity\tscore\n" > giraffe_parental_report.${SIMULATED_READ_DIR}.tsv ; \
+printf "${GRAPH}\t${GBWT}\t${READS}\t${PARAM_PRESET}\t${PAIRING}\t${CORRECT_COUNT}\t${MAPQ}\t${MAPQ60}\t${IDENTITY}\t${SCORE}\n" >> giraffe_parental_report.${SIMULATED_READ_DIR}.tsv ; \
+printf "correct\tmq\tscore\taligner\n" > giraffe_parental_roc_stats.${SIMULATED_READ_DIR}.tsv ; \
+jq -r "(if .correctly_mapped then 1 else 0 end|tostring) + \",\" + (.mapping_quality|tostring) + \",\" + (.score|tostring)" giraffe_parental.compared.json | sed "s/\,/\t/g" | sed "s/$/\tgiraffe_parental_${PARAM_PRESET}_${GRAPH}${GBWT}${READS}${PAIRING}/" >> giraffe_parental_roc_stats.${SIMULATED_READ_DIR}.tsv ; \
+sed -i "s/single//g ; s/paired/-pe/g ; s/null/0/g" giraffe_parental_roc_stats.${SIMULATED_READ_DIR}.tsv'
 
--- vg_pedigree_manuscript_methods/mapevals/sim_reads.sh
+++ vg_pedigree_manuscript_methods/mapevals/sim_reads.sh
@@ -1,23 +1,48 @@
 #!/bin/bash
 module load singularity
 cd /data/Udpbinfo/usr/markellocj/vg_trio_methods/redo_mapevals/HG002_sim_reads
-singularity shell -H ${PWD}:${HOME} --pwd ${HOME} \
--B /data/markellocj/raw_read_data/HG002_cohort_precision_fda_reads/HG002:${HOME}/HG002 \
-docker://quay.io/vgteam/vg:ci-2890-655a9622c3d60e87f14b88d943fbd8554214a975
 
-NREADS=1000000
-FASTQ=HG002/HG002_merged_interleaved-shuffled-1m.fastq.gz
+## SIMULATE BASELINE READS
+#singularity shell -H ${PWD}:${HOME} --pwd ${HOME} \
+#-B /data/markellocj/raw_read_data/HG002_cohort_precision_fda_reads/HG002:${HOME}/HG002 \
+#docker://quay.io/vgteam/vg:ci-2890-655a9622c3d60e87f14b88d943fbd8554214a975
+#
+#NREADS=1000000
+#FASTQ=HG002/HG002_merged_interleaved-shuffled-1m.fastq.gz
+#
+#echo simulate reads
+#vg sim -r -n $NREADS -a -s 12345 -p 570 -v 165 -i 0.00029 -x hg002_sample_grch38.xg -g hg002_sample_grch38.gbwt --sample-name HG002 --ploidy-regex "hs38d1:0,chrNC_007605:0,chrX:1,chrY:1,chrY_.*:1,chrEBV:0,.*:2" -F $FASTQ > sim.raw.gam
 
-echo simulate reads
-vg sim -r -n $NREADS -a -s 12345 -p 570 -v 165 -i 0.00029 -x hg002_sample_grch38.xg -g hg002_sample_grch38.gbwt --sample-name HG002 --ploidy-regex "hs38d1:0,chrNC_007605:0,chrX:1,chrY:1,chrY_.*:1,chrEBV:0,.*:2" -F $FASTQ > sim.raw.gam
+## SIMULATE REGION SPECIFIC READS
+#REGION="alllowmapandsegdupregions_hg002_v4.2.1_regions"
+#BED_FILE="HG002_GRCh38_v4.2.1.alllowmapandsegdupregions.bed"
+#REGION="cmrg_hg002_v4.2.1_regions"
+#BED_FILE="HG002_GRCh38_CMRG_smallvar_v1.00.bed"
+REGION="high_conf_NO1000GP_hg002_v4.2.1_regions"
+BED_FILE="HG002_GRCh38_CHROM1-22_v4.2.1.highconf.NO_SNP1KG.bed"
+mkdir -p ${REGION}
+cp sim.raw.gam hg002_sample_grch38.vg ${REGION}/
+cd /data/Udpbinfo/usr/markellocj/vg_trio_methods/redo_mapevals/HG002_sim_reads/${REGION}
 
-echo annotate with paths
-vg annotate -p -x graph.vg -a sim.raw.gam > sim.gam
+singularity exec --env REGION=${REGION},BED_FILE=${BED_FILE} -H ${PWD}:${HOME} --pwd ${HOME} \
+-B /data/markellocj/benchmark_data/HG002_cohort:${HOME}/HG002_cohort \
+docker://quay.io/vgteam/vg:ci-2890-655a9622c3d60e87f14b88d943fbd8554214a975 \
+/bin/bash -xc 'set -eo pipefail && \
+echo annotate with paths && \
+time vg annotate \
+-p -x hg002_sample_grch38.vg -a sim.raw.gam \
+--bed-name HG002_cohort/${BED_FILE} \
+--threads 32 > sim.${REGION}.gam && \
+time vg filter -i -U -F "" sim.${REGION}.gam > sim.gam && \
+rm -f sim.${REGION}.gam sim.fq.gz sim.fq && \
+echo convert to fastq && \
+time vg view -X -a sim.gam | gzip > sim.fq.gz && \
+gunzip sim.fq.gz && \
+sed "s/_1\$//g" sim.fq | sed "s/_2\$//g" > sim.paired.fq && \
+echo format true position information && \
+time vg view -a sim.gam | jq -c -r "[.name] + if (.annotation.features | length) > 0 then [.annotation.features | join(\",\")] else [\".\"] end + if .refpos != null then [.refpos[] | .name, if .offset != null then .offset else 0 end] else [] end + [.score] + if .mapping_quality == null then [0] else [.mapping_quality] end | @tsv" > true.pos'
 
-echo convert to fastq
-vg view -X -a sim.gam | gzip > sim.fq.gz
 
-echo format true position information
-vg view -a sim.gam | jq -c -r '[.name] + if (.annotation.features | length) > 0 then [.annotation.features | join(",")] else ["."] end + if .refpos != null then [.refpos[] | .name, if .offset != null then .offset else 0 end] else [] end + [.score] + if .mapping_quality == null then [0] else [.mapping_quality] end | @tsv' > true.pos
+
 
 
