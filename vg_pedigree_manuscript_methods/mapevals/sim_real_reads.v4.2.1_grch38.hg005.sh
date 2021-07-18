37abcab587f7b7a15b8ba5c8ba9018a5e1e20456
Charles Markello <cmarkell@ucsc.edu>
2021-07-18 18:35:10 -0400

    Backup mapeval script bug fix

--- vg_pedigree_manuscript_methods/mapevals/sim_real_reads.v4.2.1_grch38.hg005.sh
+++ vg_pedigree_manuscript_methods/mapevals/sim_real_reads.v4.2.1_grch38.hg005.sh
@@ -20,13 +20,13 @@ NREADS=1000000 ; \
 FASTQ=HG005/HG005_merged_interleaved-shuffled-1m.fastq.gz ; \
 echo simulate reads ; \
 time vg sim -r -I -n $NREADS -a -s 12345 -p 570 -v 165 -i 0.00029 -x vg-construct-v3.2.2_grch38-hg005-outstore/hg005_sample_grch38.xg -g vg-construct-v3.2.2_grch38-hg005-outstore/hg005_sample_grch38.gbwt --sample-name HG005 --ploidy-regex "hs38d1:0,chrNC_007605:0,chrX:1,chrY:1,chrY_.*:1,chrEBV:0,.*:2" -F $FASTQ > sim.raw.gam && \
-echo annotate with paths \
-time vg annotate -p -x vg-construct-v3.2.2_grch38-hg005-outstore/hg005_sample_grch38.vg -a sim.raw.gam > sim.gam && \
+echo annotate with paths ; \
+time vg annotate -p -x vg-construct-v3.2.2_grch38-hg005-outstore/hg005_sample_grch38.xg -a sim.raw.gam > sim.gam && \
 echo convert to fastq ; \
 time vg view -X -a sim.gam | gzip > sim.fq.gz && \
 gunzip sim.fq.gz && \
 sed "s/_1\$//g" sim.fq | sed "s/_2\$//g" > sim.paired.fq && \
-echo format true position information && \
+echo format true position information ; \
 time vg view -a sim.gam | jq -c -r "[.name] + if (.annotation.features | length) > 0 then [.annotation.features | join(\",\")] else [\".\"] end + if .refpos != null then [.refpos[] | .name, if .offset != null then .offset else 0 end] else [] end + [.score] + if .mapping_quality == null then [0] else [.mapping_quality] end | @tsv" > true.pos'
 
 
